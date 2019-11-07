de5e504c70d46512104b3540ddbd6e7d854b24fc
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-11-07 20:15:38 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan58-TambahKurang/src/if1/pkg10118038/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang akan menampilkan hasil dari
+ *                     perjumlahan dan selisih
+ */
+public class JumlahBilangan extends Bilangan {
+    
+    public void tampilHasilJumlah() {
+        System.out.println("Hasil Perjumlahan = " + (getX() + getY()));
+    }
+    
+}
